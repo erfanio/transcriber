102bbeaa90688f76393076cddfbfe1ed439ce93c
erfanio <dev@erfan.io>
2026-08-29 10:25:05 +1000

    chore: project setup

---
+++ transcribe-clips/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  transcribe-clips
+//
+//  Created by Erfan on 29/8/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ transcribe-clips/transcribe_clipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  transcribe_clipsApp.swift
+//  transcribe-clips
+//
+//  Created by Erfan on 29/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct transcribe_clipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
